77bccccaed2dd3f3ba32e89767c7ba513a68c7a2
utmswap <113999097+utmswap@users.noreply.github.com>
2022-09-26 23:16:04 +0530

    Add files via upload

---
+++ UTM Digital Assets.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Snapshot.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
+
+/// @custom:security-contact admin@utmswap.io
+contract UTMDigitalAssets is ERC20, ERC20Burnable, ERC20Snapshot, Ownable, Pausable {
+    constructor() ERC20("UTM Digital Assets", "UTM") {
+        _mint(msg.sender, 110000000 * 10 ** decimals());
+    }
+
+    function snapshot() public onlyOwner {
+        _snapshot();
+    }
+
+    function pause() public onlyOwner {
+        _pause();
+    }
+
+    function unpause() public onlyOwner {
+        _unpause();
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+
+    function _beforeTokenTransfer(address from, address to, uint256 amount)
+        internal
+        whenNotPaused
+        override(ERC20, ERC20Snapshot)
+    {
+        super._beforeTokenTransfer(from, to, amount);
+    }
+}
