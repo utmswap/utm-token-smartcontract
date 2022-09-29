befd4e4ebbf2100a82d729087469c424591aa034
utmswap <113999097+utmswap@users.noreply.github.com>
2022-09-29 17:38:22 +0530

    Update UTM Digital Assets.sol

--- UTM Digital Assets.sol
+++ UTM Digital Assets.sol
@@ -1,12 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Snapshot.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/security/Pausable.sol";
-
 /// @custom:security-contact admin@utmswap.io
 contract UTMDigitalAssets is ERC20, ERC20Burnable, ERC20Snapshot, Ownable, Pausable {
     constructor() ERC20("UTM Digital Assets", "UTM") {
